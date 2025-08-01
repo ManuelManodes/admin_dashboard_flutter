0bedc99f2a69c57f1fa240829bfde623d33beb17
Manuel Mañodes <manuelmanodescofre@gmail.com>
2025-07-31 22:26:34 -0400

    Sección 8: Formularios de ingreso, registro y navegación: Formulario de ingreso

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,67 +1,94 @@
+import 'package:admin_dashboard/providers/login_form_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
 import 'package:admin_dashboard/ui/buttons/link_text.dart';
 import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 100),
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      color: Colors.black,
-      child: Center(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(maxWidth: 370),
-          child: Form(
-            child: Column(
-              children: [
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  cursorColor: Colors.white, // ✅ Cursor blanco
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: 'Ingrese su correo',
-                    label: 'Correo',
-                    icon: Icons.email_outlined,
+    return ChangeNotifierProvider(
+      create: (_) => LoginFormProvider(),
+      child: Builder(
+        builder: (context) {
+          final loginFormProvider = Provider.of<LoginFormProvider>(
+            context,
+            listen: false,
+          );
+          return Container(
+            margin: EdgeInsets.only(top: 100),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            color: Colors.black,
+            child: Center(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 370),
+                child: Form(
+                  key: loginFormProvider.formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white, // ✅ Cursor blanco
+                        decoration: CustomInputs.loginInputDecoration(
+                          hint: 'Ingrese su correo',
+                          label: 'Correo',
+                          icon: Icons.email_outlined,
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty)
+                            return 'Ingrese su contraseña';
+                          if (value.length < 6)
+                            return 'La contraseña debe tener al menos 6 caracteres';
+
+                          return null;
+                        },
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                        decoration: CustomInputs.loginInputDecoration(
+                          hint: '*********',
+                          label: 'Contraseña',
+                          icon: Icons.lock_outline,
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double
+                            .infinity, // ✅ Fuerza al botón a ocupar todo el ancho
+                        child: CustomOutlinedButton(
+                          onPressed: () {
+                            loginFormProvider.validateForm();
+                          },
+                          text: 'Ingresar',
+                          color: Colors.blueGrey,
+                          isFilled: true,
+                        ),
+                      ),
+
+                      SizedBox(height: 20),
+                      LinkText(
+                        text: 'Nueva cuenta',
+                        onPressed: () {
+                          Navigator.pushNamed(
+                            context,
+                            Flurorouter.registerRoute,
+                          );
+                        },
+                      ),
+                    ],
                   ),
                 ),
-
-                SizedBox(height: 20),
-
-                TextFormField(
-                  style: TextStyle(color: Colors.white),
-                  cursorColor: Colors.white,
-                  decoration: CustomInputs.loginInputDecoration(
-                    hint: '*********',
-                    label: 'Contraseña',
-                    icon: Icons.lock_outline,
-                  ),
-                ),
-
-                SizedBox(height: 20),
-                SizedBox(
-                  width: double
-                      .infinity, // ✅ Fuerza al botón a ocupar todo el ancho
-                  child: CustomOutlinedButton(
-                    onPressed: () {},
-                    text: 'Ingresar',
-                    color: Colors.blueGrey,
-                    isFilled: true,
-                  ),
-                ),
-
-                SizedBox(height: 20),
-                LinkText(
-                  text: 'Nueva cuenta',
-                  onPressed: () {
-                    Navigator.pushNamed(context, Flurorouter.registerRoute);
-                  },
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
